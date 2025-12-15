d613528bb78ad5ca1b048193148c88e6c86975ac
Evan Liem <evan.sl1307@gmail.com>
2025-12-15 13:20:24 -0500

    added weight in class

---
+++ Backend/app/data/catalog.py
@@ -0,0 +1,4 @@
+MOCK_CARS = [
+    {"id": "civic_2018", "make": "Honda", "model": "Civic", "year": 2018, "price": 19000, "drivetrain": "FWD", "mpg": 32, "seats": 5},
+    {"id": "wrx_2018", "make": "Subaru", "model": "WRX", "year": 2018, "price": 24000, "drivetrain": "AWD", "mpg": 24, "seats": 5},
+]
--- Backend/app/models.py
+++ Backend/app/models.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Dict, Optional
 
 class CarRecommendationRequest(BaseModel):
-    budget: int = Field(gt = 0, description="The max purchasing budget")
+    budget: int = Field(gt = 0, description="The max purchasing budget") #gt= greater than 
     location: str = Field(min_length= 2, description= "The location of the user")
     annual_km : str = Field(gt = 0, desciption= "The anual km the person expects to drive")
-    passengers: int = Field(ge=1, description="Number of passengers")
-    priorities: List[str] = Field(description="User priorities for recommendation")
+    passengers: int = Field(ge=1, description="Number of passengers") #greater/equal to 
+    priorities: List[str] = Field(description="User priorities for recommendation")''
+    weights: Optional[Dict[str, float]] =Field(
+        defualt = None,
+        description="Optinal weights for priorities (0.0 to 1.0)"
+     )
+
 
