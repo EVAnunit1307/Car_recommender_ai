098cd5bff291ff82629e1bda8d02cf9e7680e32b
Evan Liem <evan.sl1307@gmail.com>
2025-12-15 02:06:06 -0500

    cleaning

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from app.services.external_apis import get_age_guess
-from app.models import AgeGuessRequest
 from app.models import CarRecommendationRequest
 from app.services.nhtsa_issues import get_complaints_and_recalls
 app = FastAPI()
@@ -11,21 +9,6 @@ def recommend_car(request: CarRecommendationRequest) -> dict:
     return {
         "message": "Recommendation logic not implemented yet",
         "input_received": request.dict()
-    }
-
-
-
-@app.post("/age-guess")
-def age_guess_post(payload: AgeGuessRequest) -> dict:
-    data = get_age_guess(payload.name)
-
-    if "error" in data:
-        return {"input_name": payload.name, "error": data["error"]}
-
-    return {
-        "input_name": payload.name,
-        "estimated_age": data.get("age"),
-        "samples": data.get("count"),
     }
 
 @app.get("/nhtsa/issues")
